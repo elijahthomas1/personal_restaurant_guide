b2249836baa8b5529eaba72baaa655356afc73a4
Harsh Patel <harshpatelhp728@gmail.com>
2022-04-18 09:46:30 -0400

    created add rest component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,43 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AddRestaurantComponent } from './add-restaurant/add-restaurant.component';
 import { InfoscreenComponent } from './infoscreen/infoscreen.component';
 import { MainscreenComponent } from './mainscreen/mainscreen.component';
 import { ViewRestaurantscreenComponent } from './view-restaurantscreen/view-restaurantscreen.component';
 
-
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'main',
-    component: MainscreenComponent
+    component: MainscreenComponent,
   },
   {
     path: 'info',
-    component: InfoscreenComponent
+    component: InfoscreenComponent,
   },
   {
     path: 'viewRestaurant',
-    component: ViewRestaurantscreenComponent
+    component: ViewRestaurantscreenComponent,
   },
-  
+  {
+    path: 'addRest',
+    component: AddRestaurantComponent,
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
