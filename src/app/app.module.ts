89317941689db0a33d3540ef8ee8499e188214ef
elijahthomas1 <thomas.elijah@sympatico.ca>
2022-04-17 21:26:12 -0400

    added item display to main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MainscreenComponent } from './mainscreen/mainscreen.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainscreenComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
--- src/app/mainscreen/mainscreen.component.ts
+++ src/app/mainscreen/mainscreen.component.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Storage } from '@capacitor/storage';
 
+
 @Component({
   selector: 'app-mainscreen',
   templateUrl: './mainscreen.component.html',
@@ -12,14 +13,54 @@ import { Storage } from '@capacitor/storage';
 })
 export class MainscreenComponent implements OnInit {
 
-  allRestaurants: any[] = [];
+  public allRestaurants: Array<any>;
+  myStar = `<ion-icon name="heart"></ion-icon>`;
+  restkey = "restaurants";
 
   constructor(private router: Router) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getAllRestaurants();
+  }
 
   goInfo() {
     this.router.navigateByUrl('/info')
+  }
+
+  getAllRestaurants() {
+    Storage.get({key:`${this.restkey}`}).then((res) => {
+      let obj = JSON.parse(res.value);
+      this.allRestaurants = obj;
+      this.allRestaurants = this.allrest;
+      this.setRestaurants()
+      console.log(this.allRestaurants)
+      
+    })
+  }
+
+
+
+  // name, location, description , tags, rating
+  rest1 = {name:"Pizza Pizza", location:"2077 Danforth Ave, Toronto", description:"Delicious Pizza and Wings", tags:["pizza", "wings","italian"],rating: "5"}
+  rest2 = {name:"McDonalds", location:"31 Woodward Ave, Toronto", description:"Big Mac Burgers and Fries", tags:["burgers", "fries","american"],rating: "4"}
+  rest3 = {name:"KFC", location:"1364 Queen St E, Toronto", description:"Fried Chicken, Sandwiches, Fries", tags:["Chicken", "wings","american"],rating: "2"}
+  allrest = [this.rest1, this.rest2, this.rest3, this.rest1, this.rest2, this.rest3]
+
+  setRestaurants() {
+    let obj = JSON.stringify(this.allRestaurants)
+    Storage.set({
+      key: `${this.restkey}`,
+      value:`${obj}`
+    }).then(() => {
+      console.log("restaurants stored")
+    })
+  }
+
+  setStars(rating) {
+    switch (rating){
+      case 5:
+        return `<ion-icon name="heart"></ion-icon>`
+    }
   }
 
 }
